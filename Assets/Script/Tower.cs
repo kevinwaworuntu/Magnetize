4afb1ebf569a2046151f9dbfed99d912d95ed14c
kevinaajw <k.waworuntu1@gmail.com>
2020-10-16 02:26:42 +0700

    First

---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+       
+    }
+    void OnMouseDown()
+    {
+        //Change tower color back to green as indicator
+        gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        playerController.closestTower = gameObject;
+
+    }
+    
+    void OnMouseUp()
+    {
+        //Change tower color back to normal as indicator
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        playerController.closestTower = null;
+    }
+}
---
+++ Assets/Script/UIControllerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIControllerScript : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject resumeBtn;
+    public GameObject restartBtn;
+    public GameObject levelClearTxt;
+    private Scene currActiveScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currActiveScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseGame();
+        }
+    }
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+        resumeBtn.SetActive(true);
+        restartBtn.SetActive(true);
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        resumeBtn.SetActive(false);
+        restartBtn.SetActive(false);
+    }
+
+
+    public void restartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currActiveScene.name);
+    }
+
+    public void endGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(false);
+        resumeBtn.SetActive(false);
+        restartBtn.SetActive(true);
+        levelClearTxt.SetActive(true);
+    }
+}
